3453c832ab8cf8b68dfe3ece6be0f4114ea974e1
nirl10 <nir2002nir@gmail.com>
2025-10-12 21:40:16 +0300

    ci test  pipeline

---
+++ tests/test_smoke.py
@@ -0,0 +1,16 @@
+import unittest
+
+from app import app
+
+
+class SmokeTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home_route_returns_ok(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
